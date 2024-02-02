d7e807a8b6fafa6e35af4f776d07dd577d228773
Yevgeny Medvedev <myjetpost@gmail.com>
2024-02-02 21:26:54 +0200

    feat(app): modify data

--- app/data.ts
+++ app/data.ts
@@ -2,16 +2,17 @@ import { v4 as uuid } from "uuid";
 
 import { BOARD_TYPE } from "./constants";
 
-export const stickers = [
-  {
-    id: uuid(),
+const firstId = uuid();
+
+export const stickers = {
+  [firstId]: {
+    id: firstId,
     top: 110,
     left: 230,
     createdAt: new Date().toISOString(),
     text: 'test draggable element',
     board: BOARD_TYPE.left
   }
-];
+};
 
-export type Sticker = Omit<typeof stickers[0], 'board'> & { board: typeof BOARD_TYPE[keyof typeof BOARD_TYPE] };
-
+export type Sticker = Omit<typeof stickers.firstId, 'board'> & { board: typeof BOARD_TYPE[keyof typeof BOARD_TYPE] };
