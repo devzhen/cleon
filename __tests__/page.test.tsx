d3f9129d55cff86a1e09de726db223fc6d020e16
Yevgeny Medvedev <myjetpost@gmail.com>
2024-02-21 11:54:44 +0200

    chore(app): small fixes

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -19,10 +19,4 @@ describe('Page', () => {
 
     expect(heading).toBeInTheDocument();
   });
-
-  it('renders homepage unchanged', () => {
-    const { container } = render(<Home />);
-
-    expect(container).toMatchSnapshot();
-  });
 });
