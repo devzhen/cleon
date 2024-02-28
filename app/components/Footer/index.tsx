d4971ac0e1d60b457fc39d322951917a7d4a26d4
Yevgeny Medvedev <myjetpost@gmail.com>
2024-02-28 23:11:49 +0200

    feat(app): add logs

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image';
 import styles from './Footer.module.css';
 
 export default function Footer() {
+  console.log(111, process.env.NEXT_PUBLIC_GITHUB_REPO);
+  console.log(222, process.env.GITHUB_REPO);
+
   return (
     <footer className={styles.footer}>
       <div className={styles.footerBg}>
