ce8725f805cf547b2f0e66b71414b53416821e8c
Yevgeny Medvedev <myjetpost@gmail.com>
2024-02-28 23:21:55 +0200

    refactor(app): remove logs

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -3,15 +3,10 @@ import Image from 'next/image';
 import styles from './Footer.module.css';
 
 export default function Footer() {
-  // eslint-disable-next-line no-console
-  console.log(111, process.env.NEXT_PUBLIC_GITHUB_REPO);
-  // eslint-disable-next-line no-console
-  console.log(222, process.env.GITHUB_REPO);
-
   return (
     <footer className={styles.footer}>
       <div className={styles.footerBg}>
-        <a href={process.env.NEXT_PUBLIC_GITHUB_REPO}>
+        <a href={process.env.GITHUB_REPO}>
           <Image src="/github-mark.png" width={24} height={24} alt="Img" draggable={false} />
           <Image src="/github-logo.png" width={54} height={24} alt="Img" draggable={false} />
           <span>go to the repo</span>
